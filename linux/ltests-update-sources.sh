d4e305acf88cf2101996561bd709c8f27a7e80e6
swaldhoer <34184299+swaldhoer@users.noreply.github.com>
2018-05-31 15:55:08 +0200

    typo in ltests and the patch seems to be wrong...

--- linux/ltests-update-sources.sh
+++ linux/ltests-update-sources.sh
@@ -2,4 +2,4 @@ CS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LUA_VER="5.3.4"
 TOP_DIR="$CS/.."
 TOP_DIR=$(readlink -f $TOP_DIR)
-cp $TOP_DIR/lua/lua-$LUA_VER-tests/ltest/* $TOP_DIR/lua/lua-$LUA_VER/src/
+cp $TOP_DIR/lua/lua-$LUA_VER-tests/ltests/* $TOP_DIR/lua/lua-$LUA_VER/src/
