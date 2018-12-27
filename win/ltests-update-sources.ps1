5ff73eb9ff6d978ced9eeb7ca079febf515f2971
swaldhoer <34184299+swaldhoer@users.noreply.github.com>
2018-12-27 19:09:26 +0100

    removed files no longer needed

--- win/ltests-update-sources.ps1
+++
@@ -1,5 +0,0 @@
-$CS=Split-Path $PSCommandPath
-$LUA_VER="5.3.4"
-$TOP_DIR=Join-Path $CS ".."
-$TOP_DIR=Resolve-Path $TOP_DIR
-Copy-Item $TOP_DIR\lua\lua-$LUA_VER-tests\ltests\* $TOP_DIR\lua\lua-$LUA_VER\src\
