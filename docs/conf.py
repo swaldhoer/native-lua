d9d435097d724020cc458a4c702cba8faaa05ee7
swaldhoer <34184299+swaldhoer@users.noreply.github.com>
2019-07-18 21:24:57 +0200

    run black on python files instead of flake8

--- docs/conf.py
+++ docs/conf.py
@@ -5,43 +5,43 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.graphviz',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    ]
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.graphviz",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
-source_suffix = '.rst'
+source_suffix = ".rst"
 
-master_doc = 'index'
+master_doc = "index"
 
-project = 'native-lua'
-copyright = '2018-2019, Stefan Waldhoer'
-author = 'Stefan Waldhoer'
+project = "native-lua"
+copyright = "2018-2019, Stefan Waldhoer"
+author = "Stefan Waldhoer"
 
-version = '0.0.1'
+version = "0.0.1"
 release = version
 
-language = 'en'
+language = "en"
 
 exclude_patterns = []
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 todo_include_todos = False
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {}
 
-htmlhelp_basename = '{}doc'.format(project)
+htmlhelp_basename = "{}doc".format(project)
