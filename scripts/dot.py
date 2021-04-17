cd3d2dbe40e6fe3293ceb919d0ed8f6454bea1b6
swaldhoer <34184299+swaldhoer@users.noreply.github.com>
2021-04-17 21:00:54 +0200

    fix windows build
    fix dot tool

--- scripts/dot.py
+++ scripts/dot.py
@@ -9,11 +9,8 @@ from waflib import Context, Utils
 def configure(conf):
     if not conf.env.DOT:
         conf.find_program("dot", var="DOT")
-        cmd_out = conf.cmd_and_log(
-            Utils.subst_vars("${DOT} -V", conf.env).split(),
-            output=Context.STDOUT,
-            quiet=Context.BOTH,
-        )
+        cmd = [conf.env.DOT[0], "-V"]
+        cmd_out = conf.cmd_and_log(cmd, output=Context.STDOUT, quiet=Context.BOTH)
         try:
             conf.env.DOT_VERSION = " ".join(cmd_out[1].decode("utf-8").split()[4:])
         except IndexError:
